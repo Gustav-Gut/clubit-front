61dcaf2dc823d7fd7d14b6d5ce567caf2e2c9749
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-03-22 22:13:50 -0300

    [CHG] Main layout now support float menu in mobile size. [ADD] Implement toast and modal confirm.

--- src/app/core/layout/main-layout/main-layout.ts
+++ src/app/core/layout/main-layout/main-layout.ts
@@ -2,11 +2,14 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
+import { MobileMenuComponent } from '../mobile-menu/mobile-menu';
+import { ToastContainerComponent } from '../../../shared/ui/toast-container/toast-container.component';
+import { ConfirmModalComponent } from '../../../shared/ui/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, Sidebar],
+  imports: [CommonModule, RouterModule, Sidebar, MobileMenuComponent, ToastContainerComponent, ConfirmModalComponent],
   templateUrl: './main-layout.html',
   styleUrls: ['./main-layout.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
